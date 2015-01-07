422c4f29ce0b629dc28337da4053bbe38dd2168d
konggas <konggas@gmail.com>
2015-01-06 23:08:12 -0500

    following direction from waggle

--- Kaggle_help.R
+++ Kaggle_help.R
@@ -1,14 +1,10 @@
 readData <- function(path.name, file.name, column.types, missing.types) {
-  urlfile<-paste(path.name,file.name,sep='')
-  print (urlfile)
-  #download.file(urlfile, 
-  #              destfile = "/tmp/test.csv", method = "curl")
-  read.csv( "~/titanic/Titanic/test.csv", colClasses=column.types,
-            na.strings=missing.types)
+  read.csv( paste(path.name, file.name, sep="") , 
+            colClasses=column.types,
+            na.strings=missing.types )
 }
 
-
-Titanic.path <- "https://raw.githubusercontent.com/konggas/Titanic/master/"
+Titanic.path <- "/Users/konggas/titanic/Titanic/"
 train.data.file <- "train.csv"
 test.data.file <- "test.csv"
 missing.types <- c("NA", "")
@@ -27,7 +23,6 @@ train.column.types <- c('integer',   # PassengerId
 )
 test.column.types <- train.column.types[-2]     # # no Survived column in test.csv
 
-
 train.raw <- readData(Titanic.path, train.data.file, 
                       train.column.types, missing.types)
 df.train <- train.raw
@@ -36,3 +31,68 @@ test.raw <- readData(Titanic.path, test.data.file,
                      test.column.types, missing.types)
 df.infer <- test.raw   
 
+#---------------------------------------------
+
+require(Amelia)
+missmap(df.train, main="Titanic Training Data - Missings Map", 
+        col=c("yellow", "black"), legend=FALSE)
+
+barplot(table(df.train$Survived),
+        names.arg = c("Perished", "Survived"),
+        main="Survived (passenger fate)", col="black")
+barplot(table(df.train$Pclass), 
+        names.arg = c("first", "second", "third"),
+        main="Pclass (passenger traveling class)", col="firebrick")
+barplot(table(df.train$Sex), main="Sex (gender)", col="darkviolet")
+hist(df.train$Age, main="Age", xlab = NULL, col="brown")
+barplot(table(df.train$SibSp), main="SibSp (siblings + spouse aboard)", 
+        col="darkblue")
+barplot(table(df.train$Parch), main="Parch (parents + kids aboard)", 
+        col="gray50")
+hist(df.train$Fare, main="Fare (fee paid for ticket[s])", xlab = NULL, 
+     col="darkgreen")
+barplot(table(df.train$Embarked), 
+        names.arg = c("Cherbourg", "Queenstown", "Southampton"),
+        main="Embarked (port of embarkation)", col="sienna")
+
+#--------------------------------------------------------------
+mosaicplot(df.train$Pclass ~ df.train$Survived, 
+           main="Passenger Fate by Traveling Class", shade=FALSE, 
+           color=TRUE, xlab="Pclass", ylab="Survived")
+mosaicplot(df.train$Sex ~ df.train$Survived, 
+           main="Passenger Fate by Gender", shade=FALSE, color=TRUE, 
+           xlab="Sex", ylab="Survived")
+boxplot(df.train$Age ~ df.train$Survived, 
+        main="Passenger Fate by Age",
+        xlab="Survived", ylab="Age")
+mosaicplot(df.train$Embarked ~ df.train$Survived, 
+           main="Passenger Fate by Port of Embarkation",
+           shade=FALSE, color=TRUE, xlab="Embarked", ylab="Survived")
+require(corrgram)
+corrgram.data <- df.train
+## change features of factor type to numeric type for inclusion on correlogram
+corrgram.data$Survived <- as.numeric(corrgram.data$Survived)
+corrgram.data$Pclass <- as.numeric(corrgram.data$Pclass)
+corrgram.data$Embarked <- revalue(corrgram.data$Embarked, 
+                                  c("C" = 1, "Q" = 2, "S" = 3))
+## generate correlogram
+corrgram.vars <- c("Survived", "Pclass", "Sex", "Age", 
+                   "SibSp", "Parch", "Fare", "Embarked")
+corrgram(corrgram.data[,corrgram.vars], order=FALSE, 
+         lower.panel=panel.ellipse, upper.panel=panel.pie, 
+         text.panel=panel.txt, main="Titanic Training Data")
+summary(df.train$Age)
+
+names(df.train)
+#-------------------------------------------------------------
+head(df.train$Name, n=10L)
+## function for extracting honorific (i.e. title) from the Name feature
+getTitle <- function(data) {
+  title.dot.start <- regexpr("\\,[A-Z ]{1,20}\\.", data$Name, TRUE)
+  title.comma.end <- title.dot.start + attr(title.dot.start, "match.length")-1
+  print(title.comma.end)
+  data$Title <- substr(data$Name, title.dot.start+2, title.comma.end-1)
+  return (data$Title)
+}   
+df.train$Title <- getTitle(df.train)
+unique(df.train$Title)
